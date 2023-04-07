61058a74bf239bebe2680678b065084a690c4ba4
1kb <1@1kb.ren>
2023-04-07 17:18:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ s = Scan('47.0.0.0/16')
 # 设置最大线程数为512 设置访问GET URL 设置超时时间为500毫秒
 s.setMaxThreads(512).setUrl('http://www.xxx.com/api/news/list').setTimeout(500).run()
 
-# 设置最大线程数为512
+# 设置最大线程数为512 机器好的、带宽大的可以上调，不是说只能最大512
 s.setMaxThreads(512)
 # 设置扫描的IP段
 s.setIp('47.112.0.0/16')
 # 设置扫描的URL
-s.setUrl('http://app-api.mayi.art/api/index/affichetypelist')
+s.setUrl('http://www.xxx.com/api/news/list')
 # 设置超时时间为500毫秒
 s.setTimeout(500)
 # 请求设置-控制反转 和 requests.request 参数一致
